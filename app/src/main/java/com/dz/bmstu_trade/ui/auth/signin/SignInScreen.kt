0cf62adfa73bc31a7a09320a481784a09371a7eb
LA-MM-ER <89412923+LA-MM-ER@users.noreply.github.com>
2024-03-19 14:11:23 +0300

    TODO-3: Sign in screen added

--- app/src/main/java/com/dz/bmstu_trade/ui/auth/signin/SignInScreen.kt
+++ app/src/main/java/com/dz/bmstu_trade/ui/auth/signin/SignInScreen.kt
@@ -1,47 +1,105 @@
 package com.dz.bmstu_trade.ui.auth.signin
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+
 
 @Composable
 fun SignInScreen(navController: NavHostController, onSignIn: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+
     ) {
-        Text(text = "Sign In Screen", style = MaterialTheme.typography.titleLarge)
+        Text(
+            text = "IMBRIDGE",
+            modifier = Modifier.padding(top = 128.dp),
+            color = Color(0xFF6A1B9A),
+            fontFamily = FontFamily.SansSerif,
+            style = TextStyle(fontSize = 40.sp),
+        )
+
+        Spacer(modifier = Modifier.height(116.dp))
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("e-mail") },
+            textStyle = TextStyle(fontSize = 16.sp),
+        )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Button(
-            onClick = {
-                onSignIn()
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        ) {
-            Text(text = "Вход")
-        }
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Пароль") },
+            textStyle = TextStyle(fontSize = 16.sp),
+        )
 
-        Button(
-            onClick = { navController.navigate("signUp") },
-            modifier = Modifier.padding(top = 16.dp)
+        Spacer(modifier = Modifier.height(180.dp))
+
+        Column(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = "Регистрация")
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /* Handle login button click */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6A1B9A))
+            ) {
+                Text("Вход", color = Color.White, fontSize = 14.sp)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /* Handle registration button click */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White, contentColor = Color(0xFF6A1B9A)),
+                border = BorderStroke(width = 1.dp, color = Color(0xFF6A1B9A))
+            ) {
+                Text("Регистрация", color = Color(0xFF6A1B9A), fontSize = 14.sp)
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Также можно",
+                modifier = Modifier.fillMaxWidth(),
+                color = Color.Black,
+                fontSize = 10.sp,
+
+                )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(), //0x0077FF
+                onClick = { /* Handle registration button click */ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White, contentColor = Color.Blue),
+                border = BorderStroke(width = 1.dp, color = Color.Blue)
+            ) {
+                Text("Войти через VK ID", color = Color.Blue, fontSize = 14.sp)
+            }
+
         }
     }
 }
